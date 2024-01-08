2726b3fe518730f101338aa95c13d502404de674
andreyka101 <andrejkvasnin236@gmail.com>
2024-01-08 17:47:31 +0300

    jujujujuuj

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ console.log("iuytre")
 const express = require('express')
 const app = express()
 
+// http://localhost:3000/
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+// http://localhost:3000/A9
+app.get('/A9', function (req, res) {
+  res.send('9pip9')
 })
 
 app.listen(3000)
