80bcbcfa36c5da701d94d336c68e5ae806fda334
andreyka101 <andrejkvasnin236@gmail.com>
2024-01-09 10:59:58 +0300

    реререререр

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express')
 const app = express()
 
 
+// статический путь
 // http://localhost:3000/
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+// статический путь
 // http://localhost:3000/A9
 app.get('/A9', function (req, res) {
   res.send('9pip9')
 })
 
+// динамический путь
 // http://localhost:3000/hello/текст
 app.get('/hello/:name', function (req, res) {
   res.send('hello ' + req.params.name)
